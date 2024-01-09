766f2f09981d99d61dd0cc01259b8b8361ac1d84
ashmi721 <ashmitak721@gmail.com>
2024-01-09 13:56:45 +0545

    practice to create how to deploy machine learning model app optimization in file

---
+++ Apps/salary_main.py
@@ -0,0 +1,11 @@
+import pickle
+import streamlit as st 
+
+with open('model.pickle','rb') as file:
+    model = pickle.load(file)
+
+year = st.text_input('Years of Exprience')
+
+if st.button('Submit'):
+    y_pred = model.predict([[int(year)]])
+    st.write(f'Your Salary must be around {y_pred[0]}')
